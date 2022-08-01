0a50f9a0a75a9a721827c819e300c904519707f6
Julanop Kamsatien <91970288+THETHEAR@users.noreply.github.com>
2022-08-01 11:07:41 +0700

    Add files via upload

---
+++ Lab2_3.java
@@ -0,0 +1,13 @@
+public class Lab2_3 {
+    public static void main(String[] args) {
+        int i =0 ,x=0;
+        while(i<20){
+            if(i<20){
+                x += i;
+            }
+            i++;
+        }
+        System.out.println(x);
+
+    }
+}
